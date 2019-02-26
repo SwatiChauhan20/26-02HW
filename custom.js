8107ce5f9d3c5458167713d3ef23a56e07f4b91b
SwatiChauhan20 <chauhan.swati104@gmail.com>
2019-02-27 00:28:01 +0530

    task complete

---
+++ custom.js
@@ -0,0 +1,11 @@
+const chipSetEl = document.querySelector('.mdc-chip-set');
+const chipSet = new MDCChipSet(chipSetEl);
+const chipSet1 = new MDCChipSet(chipSetEl);
+const chipSet2 = new MDCChipSet(chipSetEl);
+const chipSet3 = new MDCChipSet(chipSetEl);
+const chipSet4= new MDCChipSet(chipSetEl);
+
+const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+menu.open = true;
+
+const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
